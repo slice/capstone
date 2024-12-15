c97720d58d912cac6836965bca5d3febee34c537
Skip R <tinyslices@gmail.com>
2024-12-14 21:22:10 -0800

    create primitive types

---
+++ capstone-reconciler/primitives.ts
@@ -0,0 +1,1 @@
+export const Window = 'capstone$Window';
--- capstone-test/index.tsx
+++ capstone-test/index.tsx
@@ -1,5 +1,6 @@
 import CapstoneReconciler, {create} from 'capstone-reconciler';
+import {Window} from 'capstone-reconciler/primitives';
 
 console.log(':3');
 
-let root = create(<cool />);
+let root = create(<Window />);
